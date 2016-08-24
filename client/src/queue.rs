915ed429b8967f19cce80758da6bbf9c0e0b2a9e
Dean4Devil <dean4devil@paranoidlabs.org>
2016-08-24 13:06:39 +0200

    Reworked Client Queue

--- client/src/queue.rs
+++ client/src/queue.rs
@@ -1,98 +1,47 @@
 //! Client optimized Event Queue
 
 use std::io::Cursor;
+use std::io::{Read, Write};
 
 use mio;
 use mio::{TryRead, TryWrite};
 use mio::tcp::TcpStream;
 use mio::util::Slab;
 
+use protocol::ber::common::Tag;
+use protocol::ber::encode;
+
 struct Connection
 {
     socket: TcpStream,
-    token: mio::Token,
-    state: State,
+    //token: mio::Token,
+    readbuf: Vec<u8>,
+    writebuf: Vec<u8>,
 }
 
 impl mio::Handler for Connection
 {
     type Timeout = ();
-    type Message = ();
+    type Message = (i32, Tag);
 
     fn ready(&mut self, event_loop: &mut mio::EventLoop<Connection>, token: mio::Token, events: mio::EventSet)
     {
-
-        match self.state
+        if self.writebuf.len() > 0 && events.is_writable()
         {
-            State::Reading(_) =>
-            {
-                self.read(event_loop)
-            },
-            State::Writing(_) =>
-            {
-                self.write(event_loop)
-            },
-            State::Closed =>
-            {
-                event_loop.shutdown();
-            }
+            self.socket.write(self.writebuf.as_slice());
         }
-    }
-}
 
-impl Connection
-{
-    fn read(&mut self, event_loop: &mut mio::EventLoop<Connection>)
-    {
-        match self.socket.try_read_buf(self.state.mut_read_buf())
+        if events.is_readable()
         {
-            Ok(Some(0)) =>
-            {
-                // Socket is closed
-                self.state = State::Closed;
-            },
-            Ok(Some(n)) =>
-            {
-                self.reregister(event_loop)
-            },
-            Ok(None) =>
-            {
-                self.reregister(event_loop)
-            }
-            Err(e) =>
-            {
-                panic!("Got an error: {:?}", e);
-            },
+            self.socket.read_to_end(&mut self.readbuf);
         }
     }
 
-    fn write(&mut self, event_loop: &mut mio::EventLoop<Connection>)
+    fn notify(&mut self, event_loop: &mut mio::EventLoop<Connection>, msg: Self::Message)
     {
-
-    }
-
-    fn reregister(&mut self, event_loop: &mut mio::EventLoop<Connection>)
-    {
-
-    }
-}
-
-enum State
-{
-    Reading(Vec<u8>),
-    Writing(Cursor<Vec<u8>>),
-    Closed,
-}
-
-impl State
-{
-    fn mut_read_buf(&mut self) -> &mut Vec<u8>
-    {
-        match *self
+        if let Ok(bytes) = encode(msg.1, msg.0)
         {
-            State::Reading(ref mut buf) => buf,
-            // FIXME
-            _ => panic!()
+            self.writebuf.write(&bytes);
         }
     }
 }
