b74157f9748282140b0be9e4569fb9e2753e8a3d
Gregor Reitzenstein <dean4devil@paranoidlabs.org>
2017-02-22 20:35:57 +0100

    Expands LdapCodec for search

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,31 +1,31 @@
 use tokio_core::io::{Io, Codec, EasyBuf, Framed};
 use std::io;
+use std::collections::HashSet;
 
-use futures::sync::mpsc;
-use futures::{Future, Stream, Poll};
-
-use tokio_proto::streaming::{Body, Message};
-use tokio_proto::streaming::multiplex::{Frame, ClientProto, RequestId};
+use tokio_proto::streaming::multiplex::{Frame, ClientProto};
 
 use asnom::common;
 use asnom::IResult;
 use asnom::structures::{Tag, Integer, Sequence, ASNTag};
-use asnom::structure::StructureTag;
 
 use asnom::parse::{parse_tag, parse_uint};
 use asnom::write;
 
-pub struct LdapCodec;
+pub struct LdapCodec {
+    search_seen: HashSet<u64>,
+}
 
 impl Codec for LdapCodec {
     type In = Frame<Tag, Tag, io::Error>;
     type Out = Frame<Tag, Tag, io::Error>;
 
     fn decode(&mut self, buf: &mut EasyBuf) -> Result<Option<Self::In>, io::Error> {
-        match parse_tag(buf.as_slice()) {
+        let l = buf.len();
+        match parse_tag(buf.drain_to(l).as_slice()) {
             IResult::Incomplete(_) => { Ok(None)},
             IResult::Error(e) => Err(io::Error::new(io::ErrorKind::Other, e)),
             IResult::Done(_, i) => {
+                trace!("Received packet: {:?}", i);
                 if let Some(mut tags) = i.match_id(16u64).and_then(|x| x.expect_constructed()) {
                     let protoop = tags.pop().unwrap();
                     let msgid: Vec<u8> = tags.pop().unwrap()
@@ -33,17 +33,36 @@ impl Codec for LdapCodec {
                                     .and_then(|x| x.match_id(2u64))
                                     .and_then(|x| x.expect_primitive()).unwrap();
                     if let IResult::Done(_, id) = parse_uint(msgid.as_slice()) {
+                        debug!("Received Message with ID {} and ProtocolOP {:?}", id, &protoop);
                         return match protoop.id {
                             // SearchResultEntry
-                            4 => Ok(Some(Frame::Body {
-                                id: id as u64,
-                                chunk: Some(Tag::StructureTag(protoop)),
-                            })),
+                            4 => {
+                                // We have already received the first of those results, so we only
+                                // send a body frame.
+                                if self.search_seen.contains(&id) {
+                                    Ok(Some(Frame::Body {
+                                        id: id as u64,
+                                        chunk: Some(Tag::StructureTag(protoop)),
+                                    }))
+                                } // If we haven't yet seen that search, we need to initially send a whole message
+                                else {
+                                    self.search_seen.insert(id);
+                                    Ok(Some(Frame::Message {
+                                        id: id as u64,
+                                        message: Tag::StructureTag(protoop),
+                                        body: true,
+                                        solo: false,
+                                    }))
+                                }
+                            },
                             // SearchResultDone
-                            5 => Ok(Some(Frame::Body {
-                                id: id as u64,
-                                chunk: None,
-                            })),
+                            5 => {
+                                self.search_seen.remove(&id);
+                                Ok(Some(Frame::Body {
+                                    id: id as u64,
+                                    chunk: None,
+                                }))
+                            },
                             // Any other Message
                             _ => Ok(Some(Frame::Message {
                                 id: id as u64,
@@ -61,7 +80,7 @@ impl Codec for LdapCodec {
 
     fn encode(&mut self, msg: Self::Out, into: &mut Vec<u8>) -> io::Result<()> {
         match msg {
-            Frame::Message {message, id, body, solo} => {
+            Frame::Message {message, id, body: _, solo: _} => {
                 let outtag = Tag::Sequence(Sequence {
                     inner: vec![
                         Tag::Integer(Integer {
@@ -74,6 +93,7 @@ impl Codec for LdapCodec {
                 });
 
                 let outstruct = outtag.into_structure();
+                trace!("Sending packet: {:?}", &outstruct);
                 try!(write::encode_into(into, outstruct));
                 Ok(())
             },
@@ -96,6 +116,7 @@ impl<T: Io + 'static> ClientProto<T> for LdapProto {
     type BindTransport = Result<Self::Transport, io::Error>;
 
     fn bind_transport(&self, io: T) -> Self::BindTransport {
-        Ok(io.framed(LdapCodec))
+        let ldapcodec = LdapCodec { search_seen: HashSet::new() };
+        Ok(io.framed(ldapcodec))
     }
 }
